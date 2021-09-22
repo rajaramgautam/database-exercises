9330adf7df4e8f9e4f2a9181edce9bfa07f79038
Rajaram Gautam <rajaram.gautam15@gmail.com>
2021-09-21 20:35:31 -0500

    index and primary key concept

---
+++ exercise_index.sql
@@ -0,0 +1,12 @@
+# 1. USE your employees database.
+     USE employees;
+# 2. DESCRIBE each table and inspect the keys and see which columns have indexes and keys.
+desc departments;
+desc dept_emp;
+desc dept_manager;
+desc employees;
+desc salaries;
+desc titles;
+
+# Difference between index and primary keys
+
