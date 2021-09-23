9b71f582f5e8e6754f78886922efcf50cfee5cd5
Rajaram Gautam <rajaram.gautam15@gmail.com>
2021-09-23 12:02:54 -0500

    Index and Primary Key

--- exercise_index.sql
+++ exercise_index.sql
@@ -8,5 +8,15 @@ desc employees;
 desc salaries;
 desc titles;
 
+
+show create table departments;
+show create table dept_emp;
+show create table dept_manager;
+show create  table employees;
+show create table salaries;
+show create  table titles;
+
 # Difference between index and primary keys
+
+-- Primary key is a type of index. Index enhances efficiency of database.
 
