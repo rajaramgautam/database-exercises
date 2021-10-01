5d9e47f4f642a9163758a1c21e70f82ab3726ad6
Rajaram Gautam <rajaram.gautam15@gmail.com>
2021-09-30 20:29:35 -0500

    More Exercises

--- more_exercises.sql
+++ more_exercises.sql
@@ -256,33 +256,66 @@ order by life_expectancy;
 25 rows in set (0.00 sec)
 Bonus
 
-Find all the countries whose local name is different from the official name
+# 1. Find all the countries whose local name is different from the official name
+
+select country.name from country
+where name != localname;
+
+
+
 How many countries have a life expectancy less than x?
 What state is city x located in?
 What region of the world is city x located in?
 What country (use the human readable name) city x located in?
 What is the life expectancy in city x?
-Sakila Database
 
-Display the first and last names in all lowercase of all the actors.
-You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." What is one query would you could use to obtain this information?
-Find all actors whose last name contain the letters "gen":
-Find all actors whose last names contain the letters "li". This time, order the rows by last name and first name, in that order.
-Using IN, display the country_id and country columns for the following countries: Afghanistan, Bangladesh, and China:
-List the last names of all the actors, as well as how many actors have that last name.
-List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
-You cannot locate the schema of the address table. Which query would you use to re-create it?
-Use JOIN to display the first and last names, as well as the address, of each staff member.
-Use JOIN to display the total amount rung up by each staff member in August of 2005.
-List each film and the number of actors who are listed for that film.
-How many copies of the film Hunchback Impossible exist in the inventory system?
-The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters K and Q have also soared in popularity. Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
-Use subqueries to display all actors who appear in the film Alone Trip.
-You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers.
-Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as famiy films.
-Write a query to display how much business, in dollars, each store brought in.
-Write a query to display for each store its store ID, city, and country.
-List the top five genres in gross revenue in descending order. (Hint: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
+
+## Sakila Database
+
+# 1. Display the first and last names in all lowercase of all the actors.
+select * from actor;
+
+select lower(first_name) as Lower_First, lower(last_name) as Lower_Last from actor;
+
+# 2. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." What is one query would you could use to obtain this information?
+# 3. Find all actors whose last name contain the letters "gen":
+# 4. Find all actors whose last names contain the letters "li". This time, order the rows by last name and first name, in that order.
+# 5.Using IN, display the country_id and country columns for the following countries: Afghanistan, Bangladesh, and China:
+# 6. List the last names of all the actors, as well as how many actors have that last name.
+# 7. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
+# 8. You cannot locate the schema of the address table. Which query would you use to re-create it?
+# 9. Use JOIN to display the first and last names, as well as the address, of each staff member.
+# 10. Use JOIN to display the total amount rung up by each staff member in August of 2005.
+# 11. List each film and the number of actors who are listed for that film.
+# 12. How many copies of the film Hunchback Impossible exist in the inventory system?
+# 13. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters K and Q have also soared in popularity. Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
+
+select title, language_id from film
+where (title like 'k%' 
+or title like 'q%')
+and language_id =  (select language_id from language where name = 'English');
+
+
+
+
+select language_id from language
+where name = 'English';
+
+select * from film;
+
+# 14. Use subqueries to display all actors who appear in the film Alone Trip.
+select * from film;
+select * from film_actor;
+select * from actor;
+
+select first_name, last_name from 
+
+
+# 15. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers.
+# 16. Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as famiy films.
+# 17. Write a query to display how much business, in dollars, each store brought in.
+# 18. Write a query to display for each store its store ID, city, and country.
+# 19. List the top five genres in gross revenue in descending order. (Hint: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
 SELECT statements
 
 Select all columns from the actor table.
